540e1799abc3518672fb4a3252335a9ed5a1d73d
hybutalla <hybutalla@LAPTOP-CGHN4N3K>
2020-02-15 12:15:43 -0500

    create

---
+++ MavenFramework2/src/main/java/MavenFramework/MavenFramework2/testgit.java
@@ -0,0 +1,10 @@
+package MavenFramework.MavenFramework2;
+
+public class testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
